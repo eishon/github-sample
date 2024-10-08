e5d916ae08ee15204038994b7a2212a2e51820ee
Ahmed Fahim Foysal Eishon <eishon.dev@gmail.com>
2024-10-08 21:44:12 +0900

    some UI refactor with dark theme color switch

--- app/src/main/java/com/arctrix/githubsample/ui/home/HomeScreen.kt
+++ app/src/main/java/com/arctrix/githubsample/ui/home/HomeScreen.kt
@@ -1,13 +1,19 @@
 package com.arctrix.githubsample.ui.home
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.CircularProgressIndicator
@@ -26,6 +32,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.isTraversalGroup
@@ -42,6 +49,8 @@ import com.arctrix.githubsample.ui.common.theme.GithubSampleTheme
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(viewModel: HomeViewModel = viewModel()) {
+    val isDarkTheme = isSystemInDarkTheme()
+    val backgroundColor = if (isDarkTheme) Color.Black else Color.White
 
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
 
@@ -54,9 +63,11 @@ fun HomeScreen(viewModel: HomeViewModel = viewModel()) {
     }
 
     Column(
-        Modifier
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
             .fillMaxSize()
-            .semantics { isTraversalGroup = true }) {
+            .semantics { isTraversalGroup = true }
+    ) {
         SearchBar(
             modifier = Modifier
                 .align(Alignment.CenterHorizontally)
@@ -81,6 +92,8 @@ fun HomeScreen(viewModel: HomeViewModel = viewModel()) {
             // implement for search suggestions
         }
 
+        Spacer(modifier = Modifier.height(16.dp))
+
         if (uiState.isLoading) {
             CircularProgressIndicator()
         } else if (uiState.error.isNullOrEmpty().not()) {
@@ -95,22 +108,35 @@ fun HomeScreen(viewModel: HomeViewModel = viewModel()) {
             ) {
                 val list = uiState.users
                 items(count = list.size) {
-                    ListItem(headlineContent =
-                    { Text(list[it].login) },
+                    ListItem(
+                        headlineContent = {
+                            Text(list[it].login)
+                        },
                         supportingContent = { Text("Additional info") },
                         leadingContent = {
                             AsyncImage(
                                 model = list[it].avatarUrl,
                                 contentDescription = null,
+                                modifier = Modifier
+                                    .size(64.dp)
+                                    .clip(RoundedCornerShape(16.dp)) // Apply rounded corners
+                                    .border(
+                                        2.dp,
+                                        Color.Gray,
+                                        RoundedCornerShape(16.dp)
+                                    ) // Apply border with rounded corners
                             )
                         },
-                        colors = ListItemDefaults.colors(containerColor = Color.Transparent),
+                        colors = ListItemDefaults.colors(containerColor = backgroundColor),
                         modifier = Modifier
                             .clickable {
                                 // Navigate to user details screen
                             }
                             .fillMaxWidth()
+                            .height(96.dp)
                             .padding(horizontal = 16.dp, vertical = 4.dp)
+                            .clip(RoundedCornerShape(16.dp)) // Apply rounded corners
+                            .border(2.dp, Color.Gray, RoundedCornerShape(16.dp))
                     )
                 }
             }
