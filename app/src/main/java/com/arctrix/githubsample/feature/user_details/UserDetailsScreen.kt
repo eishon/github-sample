e6376c05617ed09d5c6e25ac111759048cb74988
Ahmed Fahim Foysal Eishon <eishon.dev@gmail.com>
2024-10-09 01:45:57 +0900

    minor UI refactor and remove unused fields from UserDetails

--- app/src/main/java/com/arctrix/githubsample/feature/user_details/UserDetailsScreen.kt
+++ app/src/main/java/com/arctrix/githubsample/feature/user_details/UserDetailsScreen.kt
@@ -4,26 +4,19 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -199,9 +192,10 @@ fun UserProfile(
 @Composable
 fun TextWithIcon(image: ImageVector, text: String, contentColor: Color) {
     Row(
-        modifier = Modifier.width(200.dp),
+        modifier = Modifier.fillMaxWidth(0.5f),
         verticalAlignment = Alignment.CenterVertically,
     ) {
+        HorizontalSpace(8)
         Icon(
             imageVector = image,
             contentDescription = text,
@@ -209,6 +203,7 @@ fun TextWithIcon(image: ImageVector, text: String, contentColor: Color) {
         )
         HorizontalSpace(4)
         InfoText(text = text)
+        HorizontalSpace(8)
     }
 }
 
